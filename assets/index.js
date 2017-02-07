9823a149891ea846e26063e9a7079ba40edc3862
Chris W Jones <chris@christopherjones.us>
2017-02-07 18:13:49 -0500

    Almost works like a calculator
    I thought I might need to implement a RDS for this and I still might but for
    now, I'm getting by without one.  The calculator works for a single calculation
    (unless that's a divide first).  I think I may need to look into a stack-based
    calculator for this project.  That's a thing, right?

--- assets/index.js
+++ assets/index.js
@@ -1,25 +1,84 @@
+total = 0;
+lastNumber = "";
+lastOperation = "";
 $(function() {
   clearOutput();
   $("button").on("click", function(){
     currentOutput = $("#output").val();
     buttonValue = $(this).text();
+
     switch(buttonValue){
       case "ac":
         clearOutput();
+        total = 0;
+        clearLast();
         break;
-      case ".":
-        if(!currentOutput.includes(".")){
-          setOutput(currentOutput + buttonValue);
-        }
+      case "+":
+        setOutput(appendSymbol("+", currentOutput));
+        lastOperation = "ADD";
+        total = doOperation(lastOperation, total, Number.parseFloat(lastNumber));
+        clearLast();
+        break;
+      case "-":
+        setOutput(appendSymbol("-", currentOutput));
+        lastOperation = "SUB";
+        total = doOperation(lastOperation, total, Number.parseFloat(lastNumber));
+        clearLast();
+        break;
+      case "/":
+        setOutput(appendSymbol("/", currentOutput));
+        lastOperation = "DIV";
+        total = doOperation(lastOperation, total, Number.parseFloat(lastNumber));
+        clearLast();
+        break;
+      case "x":
+        setOutput(appendSymbol("x", currentOutput));
+        lastOperation = "MULT";
+        total = doOperation(lastOperation, total, Number.parseFloat(lastNumber));
+        clearLast();
         break;
       case "=":
+        total = doOperation(lastOperation, total, Number.parseFloat(lastNumber));
+        clearLast();
+        clearOutput();
+        setOutput(total);
         break;
       default:
         setOutput(currentOutput + buttonValue);
+        lastNumber = lastNumber + buttonValue;
     }
+    logIt("lastNumber: " + lastNumber);
+    logIt("lastOperation: " + lastOperation);
+    logIt("total: " + total);
   })
 });
 
+function doOperation(operand, runningTotal, number){
+  switch(operand){
+    case "ADD":
+      return runningTotal + number;
+    case "SUB":
+      return runningTotal - number;
+    case "MULT":
+      return runningTotal * number;
+    case "DIV":
+      return runningTotal / number;
+    default:
+      return runningTotal;
+   }
+}
+
+function appendSymbol(symbol, str){
+  if(str.match(/x|\+|\/|-/) || str.length == 0){
+    return str
+  } else {
+    return str + symbol;
+  }
+}
+
+function clearLast(){
+  lastNumber = "";
+}
 
 function setOutput(value){
   $("#output").val(value);
